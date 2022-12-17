83507e4574b94d9e6b072f2d2db3cb38691389db
Prajeevan <prajee.bajee@gmail.com>
2022-12-17 19:38:28 +0530

    feat: add qr scanner function

--- source/run.py
+++ source/run.py
@@ -13,21 +13,20 @@ qcd = cv2.QRCodeDetector()
 cap = cv2.VideoCapture(camera_id)
 
 # Create voice object
-def initalize_pyttsx3():
+def initialize_pyttsx3():
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
+    engine.say()
+    engine.runAndWait()
 
 # Connect with FireStore
 def connect_with_firestore():
     credentials_firestore = credentials.Certificate(r"credentials/firebase-auth-file.json")
     credentials_firestore_app = firebase_admin.initialize_app(credentials_firestore)
     firestore_database = firestore.client()    
+    return firestore_database
 
-# Convert text to speech function.
-def talk_function(text_data):
-    engine.say(text_data)
-    engine.runAndWait()
 
 # Current time return function
 def get_current_time_data():
@@ -36,28 +35,73 @@ def get_current_time_data():
     current_date = current_date_and_time.strftime("%Y-%m-%d")
     current_hour = current_date_and_time.strftime("%H")
 
-    return {"current_time":current_date, "current_date":current_date, "current_hour":current_hour}
+    return {current_time, current_date, current_hour}
+
+    
 
 
-def scanner_function():
+def scanner_function(database):
+
+    import cv2
+
+    camera_id = 0
+    delay = 1
+    window_name = 'qRec'
+
+    qcd = cv2.QRCodeDetector()
+    cap = cv2.VideoCapture(camera_id)
 
     while True:
-         ret, frame = cap.read()
-         
-         
-         cv2.imshow('Input', frame)
-         c = cv2.waitKey(1)
-         
-         if c == 27:
+        ret, frame = cap.read()
+
+        if ret:
+            ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(frame)
+            if ret_qr:
+                for s, p in zip(decoded_info, points):
+                    if s:
+                        print(s)
+                        color = (0, 255, 0)
+                        # Retrieve data 
+                        time_now, date_now, hour_now = get_current_time_data()
+                        staff_ref = database.collection(u'staffs')
+                        staff_query = staff_ref.where(u'staff', u'==', str(s)).get()
+                        if (staff_query == [] ):
+                            print("wrong")
+                            # initalize_pyttsx3()
+                        else:
+
+                            name = staff_query[0].to_dict()['fname']
+                            id = staff_query[0].to_dict()['staff']
+                            department = staff_query[0].to_dict()['department']
+                            print(name) 
+                            # if (int(hour_now) < 13):
+                            #     initialize_pyttsx3()
+                            # elif (int(hour_now)> 13):
+                            #     initialize_pyttsx3()
+                            # Input attendance 
+                        
+                            # doc_ref = database.collection(u'attendance').document(date_now + " "+ str(name))
+                            # doc_ref.set({
+                            #     u'name':str(name),
+                            #     u'present':True,
+                            #     u'time': time_now,
+                            #     u'StaffID': id,
+                            #     u'department':department,
+                            #     u'Date':date_now
+                            #     })
+                    else:
+                        color = (0, 0, 255)
+                    frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
+            cv2.imshow(window_name, frame)
+
+        if cv2.waitKey(delay) & 0xFF == ord('q'):
             break
 
-    cap.release()
-    cv2.destroyAllWindows()
-
+    cv2.destroyWindow(window_name)
 
 
 
 if __name__=='__main__':
-    initalize_pyttsx3()
-    connect_with_firestore()
-    scanner_function()
+    # initalize_pyttsx3()
+    database = connect_with_firestore()
+    scanner_function(database)
